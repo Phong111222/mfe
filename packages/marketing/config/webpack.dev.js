7ef4b2303bee4d7ec6b17f024a5009141d4a9e92
Tien Phong <phong.nguyentien@saigontechnology.com>
2024-05-26 19:06:52 +0700

    Created container deploy script

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -2,8 +2,6 @@ const { merge } = require('webpack-merge');
 
 const commonConfig = require('./webpack.common');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 const ModuleFederation = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
 
@@ -23,10 +21,6 @@ const devConfig = {
         './MarketingApp': './src/bootstrap',
       },
       shared: packageJson.dependencies,
-    }),
-
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
     }),
   ],
 };
