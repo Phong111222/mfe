76147a92f7093ddcc360f9c1829943f2329cdf8e
Frank Nguyen <frank.nguyen@ansarada.com>
2024-06-03 19:36:28 +0700

    added generated classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,13 +2,24 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+const generatedClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
+
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generatedClassName={generatedClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
